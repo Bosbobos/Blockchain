100e04e62e96178aa7137f990b9510d57bea357e
bosbobos <konovalov.matvej@mail.ru>
2025-05-27 18:08:38 +0300

    Реализовал не до конца рабочую версию подписи Шнорра

---
+++ SchnorrSignature.py
@@ -0,0 +1,33 @@
+from random import randint
+import hashlib
+
+class SchnorrSiganture:
+    def __init__(self, p, q, g):
+        self.p = p
+        self.q = q
+        self.g = g
+        self.x = randint(2, q-1)
+        self.pub_key = pow(self.g, self.x, self.p)
+
+    def get_pub_key(self):
+        return self.pub_key
+
+    def generate_sig(self, bin_message):
+        r = randint(2, self.q-1)
+        R = pow(self.g, r, self.p)
+        e = SchnorrSiganture.find_e(R, self.pub_key, bin_message)
+        s = (r + e*self.x) % self.q
+
+        return R, s
+
+
+    @staticmethod
+    def find_e(R, pub_key, bin_msg):
+        full_message = bin(R)[2:] + bin(pub_key)[2:] + bin_msg
+        return int.from_bytes(hashlib.sha256(full_message.encode()).digest())
+
+    @staticmethod
+    def verify_sig(bin_msg, sig, pub_key, g, p):
+        R, s = sig
+        e = SchnorrSiganture.find_e(R, pub_key, bin_msg)
+        return R * pow(pub_key, e, p) == pow(g, s, p)
--- Test.py
+++ Test.py
@@ -1,4 +1,5 @@
 from MerkleTree import MerkleTree
+from SchnorrSignature import SchnorrSiganture
 
 if __name__ == '__main__':
     transactions = ["tx1", "tx2", "tx3", "tx4"]
@@ -7,3 +8,10 @@ if __name__ == '__main__':
     print('\n\nAfter adding another node')
     mt.add_transaction('tx5')
     print(mt)
+
+    p, q, g = 23, 11, 2
+    sch = SchnorrSiganture(p, q, g)
+    msg = '1101'
+    sig = sch.generate_sig(msg)
+    print(SchnorrSiganture.verify_sig(msg, sig, sch.get_pub_key(), g, p))
+    print(SchnorrSiganture.verify_sig('1001', sig, sch.get_pub_key(), g, p))
